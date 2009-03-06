0bf91217445f7d56331218df6d833349b81b3756
kurilo <kurilo@db934cc0-f4af-11dd-802b-177316e4ac06>
2009-03-06 15:57:56 +0000

    Initial version

---
+++ tools/smisubtree.c
@@ -0,0 +1,48 @@
+/*
+ * smisubstree.c --
+ *
+ *      A simple SMI example program, printing an OID's subtree.
+ *
+ * Copyright (c) 1999 Frank Strauss, Technical University of Braunschweig.
+ *
+ * See the file "COPYING" for information on usage and redistribution
+ * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+ *
+ * @(#) $Id: smisubtree.c 861 2000-03-30 09:29:02Z strauss $
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <smi.h>
+ 
+int main(int argc, char *argv[])
+{
+    SmiNode *smiNode;
+    int oidlen, first = 1;
+    
+    if (argc != 2) {
+        fprintf(stderr, "Usage: smisubtree oid\n");
+        exit(1);
+    }
+ 
+    smiInit(NULL);
+
+    smiNode = smiGetNode(NULL, argv[1]);
+    if (! smiNode) {
+	fprintf(stderr, "smisubtree: node `%s' not found\n", argv[1]);
+	exit(2);
+    }
+ 
+    for (oidlen = smiNode->oidlen;
+	 smiNode && (first || smiNode->oidlen > oidlen);
+	 smiNode = smiGetNextNode(smiNode, SMI_NODEKIND_ANY), first = 0) {
+	
+        printf("%*s%-32s\n",
+	       (smiNode->oidlen - oidlen + 1) * 2, "",
+	       smiNode->name);
+        
+    };
+
+    smiExit();
+    exit(0);
+}
---
+++ tools/smitree.c
@@ -0,0 +1,44 @@
+/*
+ * smitree.c --
+ *
+ *      A simple SMI example program, printing a module's OID tree.
+ *
+ * Copyright (c) 1999 Frank Strauss, Technical University of Braunschweig.
+ *
+ * See the file "COPYING" for information on usage and redistribution
+ * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+ *
+ * @(#) $Id: smitree.c 827 2000-02-24 15:29:57Z strauss $
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <smi.h>
+
+int main(int argc, char *argv[])
+{
+    SmiNode *smiNode;
+    SmiModule *smiModule;
+    
+    if (argc != 2) {
+        fprintf(stderr, "Usage: smitree module\n");
+        exit(1);
+    }
+ 
+    smiInit("smitree");
+    smiLoadModule(argv[1]);
+    smiModule = smiGetModule(argv[1]);
+ 
+    for(smiNode = smiGetFirstNode(smiModule, SMI_NODEKIND_ANY);
+	smiNode;
+        smiNode = smiGetNextNode(smiNode, SMI_NODEKIND_ANY)) {
+
+        printf("%*s%s::%s\n", smiNode->oidlen * 2, "",
+               smiModule->name, smiNode->name);
+        
+    };
+
+    smiExit();
+    return 0;
+}
+
